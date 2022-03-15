d70dc6c058b0075ceb3427e2938b8248f12cc7d5
Simone <startae14@gmail.com>
2022-03-15 17:27:29 +0100

    Restrict access to some endpoints using the cookie session

--- server.js
+++ server.js
@@ -58,7 +58,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 function checkLoggedIn(req, res, next) {
-  const isLoggedIn = true; // TODO
+  console.log('Current user is:', req.user);
+  const isLoggedIn = req.isAuthenticated() && req.user;
   if (!isLoggedIn) {
     return res.status(401).json({
       error: 'You must log in!',
